109ac5e9640250857d09c32e33b68ebfa15cc546
Pokidova <Danem2@mail.ru>
2022-11-27 11:44:46 +0300

    initial commit

---
+++ src/test/java/ru/netology/data/User.java
@@ -0,0 +1,12 @@
+package ru.netology.data;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class User {
+    private String id;
+    private String login;
+    private String password;
+    private String status;
+}
